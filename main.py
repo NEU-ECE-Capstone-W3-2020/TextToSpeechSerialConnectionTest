11bb331bdbdd6b711194464fe828621ebb212b30
Dan <pelusodan@gmail.com>
2021-02-03 22:03:06 -0500

    adding arduino and python code

---
+++ main.py
@@ -0,0 +1,11 @@
+import serial
+import time
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser.flush()
+
+    while True:
+        inputToSpeak = input("Enter the text to speak\n")
+        print("Sending to Arduino...")
+        ser.write(bytes(inputToSpeak, 'utf-8'))
